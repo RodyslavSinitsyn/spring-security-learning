e634b6d636a618e5b5c31ac5568b8c8b7d714fe7
Rodyslav Sinitsyn <rodyslav.sinitsyn@raiffeisen.ua>
2024-06-05 23:18:01 +0300

    Init commit

---
+++ src/main/java/org/rsinitsyn/SpringSecurityLearningApplication.java
@@ -0,0 +1,12 @@
+package org.rsinitsyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityLearningApplication.class, args);
+    }
+}
