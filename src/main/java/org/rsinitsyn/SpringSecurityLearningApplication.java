9d30d9150e9223de9ebd8fb8509ee2d3bb71ce45
Rodyslav Sinitsyn <rodyslav.sinitsyn@raiffeisen.ua>
2024-06-05 23:19:38 +0300

    Basic auth

--- src/main/java/org/rsinitsyn/SpringSecurityLearningApplication.java
+++ src/main/java/org/rsinitsyn/SpringSecurityLearningApplication.java
@@ -2,11 +2,23 @@ package org.rsinitsyn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class SpringSecurityLearningApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityLearningApplication.class, args);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic(Customizer.withDefaults())
+                .authorizeRequests(auth -> auth.anyRequest().authenticated())
+                .build();
     }
 }
