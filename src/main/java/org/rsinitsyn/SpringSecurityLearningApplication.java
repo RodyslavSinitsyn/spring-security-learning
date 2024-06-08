a86790de8caf6942830d9a236e63036cefb20c24
Rodyslav Sinitsyn <rodyslav.sinitsyn@raiffeisen.ua>
2024-06-08 09:56:23 +0300

    Keycloak integration

--- src/main/java/org/rsinitsyn/SpringSecurityLearningApplication.java
+++ src/main/java/org/rsinitsyn/SpringSecurityLearningApplication.java
@@ -2,7 +2,24 @@ package org.rsinitsyn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
 
 @EnableWebSecurity(debug = true)
 @SpringBootApplication
@@ -10,5 +27,52 @@ public class SpringSecurityLearningApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityLearningApplication.class, args);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .oauth2Login(Customizer.withDefaults())
+                .authorizeHttpRequests(config -> config
+                        .requestMatchers("/error").permitAll()
+                        .requestMatchers("/manager.html").hasRole("MANAGER")
+                        .anyRequest().authenticated())
+                .build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var converter = new JwtAuthenticationConverter();
+        var authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        converter.setPrincipalClaimName("preferred_username");
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            var authorities = authoritiesConverter.convert(jwt);
+            var roles = jwt.getClaimAsStringList("spring_sec_roles");
+            return mergeAuthorities(authorities, roles);
+        });
+        return converter;
+    }
+
+    @Bean
+    public OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+        var service = new OidcUserService();
+        return userRequest -> {
+            var oidcUser = service.loadUser(userRequest);
+            var roles = oidcUser.getClaimAsStringList("spring_sec_roles");
+            var grantedAuthorities = mergeAuthorities(oidcUser.getAuthorities(), roles);
+            return new DefaultOidcUser(grantedAuthorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
+        };
+    }
+
+    private static List<GrantedAuthority> mergeAuthorities(Collection<? extends GrantedAuthority> grantedAuthorities,
+                                                           List<String> roles) {
+        return Stream.concat(
+                        grantedAuthorities.stream(),
+                        roles.stream()
+                                .filter(role -> role.startsWith("ROLE"))
+                                .map(SimpleGrantedAuthority::new)
+                                .map(GrantedAuthority.class::cast))
+                .toList();
     }
 }
